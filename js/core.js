e281424e2ff7b7ddc1d4c200b1e5ede50260cb6c
Aelina00 <sugar.w.0iiol@gmail.com>
2023-11-22 10:55:23 +0300

    added  FUNCTION FOR JS

--- js/core.js
+++ js/core.js
@@ -1,3 +1,5 @@
 document.querySelector('.js-btn').addEventListener('click', function(){
     document.querySelector('.js-btn').classList.add('active')
+    document.querySelector('.js-btn').style.height = "200px"
+    document.querySelector('.js-btn').textContent = "Текст кнопки изменен"
 })
